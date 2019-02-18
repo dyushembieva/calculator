96daed4e994db9578f5534dd6ed633c099aebe45
user <user@PC-HEAD-200.local>
2019-02-18 17:27:20 +0600

    1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class Calculator extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="calculator">
+                    <input className="calculator__display-result" id="display"/>
+                    <br/>
+                    <input className="calculator__display-input"/>
+                    <br/>
+                    <button className="calculator__clear" id="clear">AC</button>
+                    <button className="math calculator__divide" id="divide">/</button>
+                    <button className="math calculator__multiply" id="multiply">x</button>
+                    <br/>
+                    <button className="btn calculator__number-seven" id="seven">7</button>
+                    <button className="btn calculator__number-eight" id="eight">8</button>
+                    <button className="btn calculator__number-nine" id="nine">9</button>
+                    <button className="math calculator__operator-minus" id="subtract">-</button>
+                    <br/>
+                    <button className="btn calculator__number-four" id="four">4</button>
+                    <button className="btn calculator__number-five" id="five">5</button>
+                    <button className="btn calculator__number-six" id="six">6</button>
+                    <button className="math calculator__operator-minus" id="add">+</button>
+                    <br/>
+                    <button className="btn calculator__number-one" id="one">1</button>
+                    <button className="btn calculator__number-two" id="two">2</button>
+                    <button className="btn calculator__number-three" id="three">3</button>
+                    <button className="calculator__operator-equal" id="equals">=</button>
+                    <br/>
+                    <button className="calculator__number-zero" id="zero">0</button>
+                    <button className="btn calculator__symbol" id="decimal">.</button>
+                </div>
+            </div>
+        )
+    };
 }
-
-export default App;
+export default Calculator;
